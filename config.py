cef43669e789382f21d406cf3524f1724aa93d1e
sanju9636 <141401968+sanju9636@users.noreply.github.com>
2023-08-22 15:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/877ca1256330fc395b79f.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/3386b120fd7fca300a236.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/56d1760224589ee370186.jpg"
+            START_IMG_URL = "https://telegra.ph/file/3386b120fd7fca300a236.jpg"
